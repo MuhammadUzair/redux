f27de25bca46838867a0240a31128aef65c862f9
uzair <uzair@techappssoltions.com>
2017-04-24 20:28:04 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { INCREMENT, DECREMENT, RESET } from './counter';
+import {Observable} from 'rxjs/Rx';
+
+interface AppState {
+  counter: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  counter: Observable<number>;
+
+	constructor(private store: Store<AppState>){
+		this.counter = store.select('counter');
+	}
+
+	increment(){
+		this.store.dispatch({ type: INCREMENT });
+	}
+
+	decrement(){
+		this.store.dispatch({ type: DECREMENT });
+	}
+
+	reset(){
+		this.store.dispatch({ type: RESET });
+	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +17,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    StoreModule.provideStore({ counter: counterReducer }),
+    StoreDevtoolsModule.instrumentStore({
+      maxAge: 5,
+    })
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
